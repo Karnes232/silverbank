f775564eaccdbe434576ceb57b7a6e647ad31f1b
James Karnes <karnes.james@gmail.com>
2021-09-25 17:23:22 -0400

    testing photo gallery error

--- src/data/photoGallery.js
+++ src/data/photoGallery.js
@@ -406,121 +406,121 @@ export const items = [
       width: 2,
     }
   },
-  // { // Video Item:
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/_6p0J6F35PE?playlist=_6p0J6F35PE&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/nbDBVOXrx6Q?playlist=nbDBVOXrx6Q&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/ZNmHdvCqwIk?playlist=ZNmHdvCqwIk&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/gnO_6fVV7Yc?playlist=gnO_6fVV7Yc&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/wYEZJ2Qic98?playlist=wYEZJ2Qic98&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/y0A4S5DsFlQ?playlist=y0A4S5DsFlQ&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/xgLH9SJb3NM?playlist=xgLH9SJb3NM&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/e_C5_Y_4TXY?playlist=e_C5_Y_4TXY&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/F8WFs1MQ_0w?playlist=F8WFs1MQ_0w&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/Yb7J_nW2gGU?playlist=Yb7J_nW2gGU&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/l2apZkNN8-c?playlist=l2apZkNN8-c&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/ck_I2aa_qbo?playlist=ck_I2aa_qbo&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  // { 
-  //   itemId: nextId(),
-  //   mediaUrl: 'https://www.youtube.com/embed/dkxwsJ78Lfc?playlist=dkxwsJ78Lfc&loop=1',
-  //   metaData: {
-  //     type: 'video',
-  //     height: 1,
-  //     width: 2,
-  //   }
-  // },
-  ]//.sort(() => Math.random() - 0.5);
+  { // Video Item:
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/_6p0J6F35PE?playlist=_6p0J6F35PE&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/nbDBVOXrx6Q?playlist=nbDBVOXrx6Q&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/ZNmHdvCqwIk?playlist=ZNmHdvCqwIk&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/gnO_6fVV7Yc?playlist=gnO_6fVV7Yc&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/wYEZJ2Qic98?playlist=wYEZJ2Qic98&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/y0A4S5DsFlQ?playlist=y0A4S5DsFlQ&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/xgLH9SJb3NM?playlist=xgLH9SJb3NM&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/e_C5_Y_4TXY?playlist=e_C5_Y_4TXY&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/F8WFs1MQ_0w?playlist=F8WFs1MQ_0w&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/Yb7J_nW2gGU?playlist=Yb7J_nW2gGU&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/l2apZkNN8-c?playlist=l2apZkNN8-c&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/ck_I2aa_qbo?playlist=ck_I2aa_qbo&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  { 
+    itemId: nextId(),
+    mediaUrl: 'https://www.youtube.com/embed/dkxwsJ78Lfc?playlist=dkxwsJ78Lfc&loop=1',
+    metaData: {
+      type: 'video',
+      height: 1,
+      width: 2,
+    }
+  },
+  ].sort(() => Math.random() - 0.5);
