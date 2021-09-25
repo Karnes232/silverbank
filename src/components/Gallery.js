0c3d866a73f811ceb13c7ee7432652b10285dfb4
James Karnes <karnes.james@gmail.com>
2021-09-25 18:25:06 -0400

    testing gallery fix

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ProGallery } from 'pro-gallery';
 import 'pro-gallery/dist/statics/main.css';
 import { items } from "../data/photoGallery";
@@ -6,6 +6,8 @@ import { useWindowHeight, useWindowWidth } from '../data/windowDimensions';
 
 const Gallery = () => {
     //"pro-gallery": "^3.1.38"
+    const [width, setWidth] = useState(0)
+    const [height, setHeight] = useState(0)
 
     // The options of the gallery (from the playground current state)
     const options = {
@@ -13,9 +15,16 @@ const Gallery = () => {
     };
       
     // The size of the gallery container. The images will fit themselves in it
+
+    useEffect(() => {
+        if (typeof window !== undefined) {
+            setWidth(window.innerWidth)
+            setHeight(window.innerHeight)
+        }
+    }, [])
     const container = {
-        width: useWindowWidth(),
-        height: useWindowHeight()
+        width: width,
+        height: height
     };
       
     // The eventsListener will notify you anytime something has happened in the gallery.
