546cba1f1ec28e26c6b61d7dbbf7c2e5534f9e33
James Karnes <karnes.james@gmail.com>
2021-09-25 17:02:05 -0400

    testing photo gallery error

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { ProGallery } from 'pro-gallery';
 import 'pro-gallery/dist/statics/main.css';
-import { items } from "../data/photoGallery";
+//import { items } from "../data/photoGallery";
 import { useWindowHeight, useWindowWidth } from '../data/windowDimensions';
-
+import nextId from "react-id-generator";
 const Gallery = () => {
     //"pro-gallery": "^3.1.38"
 
@@ -22,6 +22,44 @@ const Gallery = () => {
     //const eventsListener = (eventName, eventData) => console.log({eventName, eventData}); 
       
     // The scrollingElement is usually the window, if you are scrolling inside another element, supply it here
+
+    const items = [
+        { // Image Item:
+          itemId: nextId(),
+          mediaUrl: 'https://i.ibb.co/NL5c439/IMG-8190.jpg',
+          metaData: {
+            type: 'image',
+            height: 1,
+            width: 2,
+          }
+        },
+        { 
+          itemId: nextId(),
+          mediaUrl: 'https://i.ibb.co/tbXw0N6/IMG-8187.jpg',
+          metaData: {
+            type: 'image',
+            height: 1,
+            width: 2,
+          }
+        },
+        {
+          itemId: nextId(),
+          mediaUrl: 'https://i.ibb.co/QY18cxX/IMG-8172.jpg',
+          metaData: {
+            type: 'image',
+            height: 1,
+            width: 2,
+          }
+        },
+        {
+          itemId: nextId(),
+          mediaUrl: 'https://i.ibb.co/g47KJnx/IMG-8162.jpg',
+          metaData: {
+            type: 'image',
+            height: 1,
+            width: 2,
+          }
+        }]
 
     
     return (
